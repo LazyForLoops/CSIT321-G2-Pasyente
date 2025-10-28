424a23571a18c1eecf5aba314516cc6730bf69db
LAB24_USER ON WS41 <L24Y10W41@LabHCI.net>
2025-10-28 12:59:59 +0800

    feature(entity): Add Medical Records Entity

---
+++ src/main/java/com/csit321/appdev/pasyente/entity/MedicalRecords.java
@@ -0,0 +1,36 @@
+package com.csit321.appdev.pasyente.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "medical_records")
+public class MedicalRecords {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long recordID;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "patient_id")
+    private Patient patient;
+    @Column(name = "description")
+    private String description;
+
+    public void setRecordID(Long recordID){
+        this.recordID = recordID;
+    }
+    public Long getRecordID(){
+        return recordID;
+    }
+    public void setDescription(String description){
+        this.description = description;
+    }
+    public String getDescription(){
+        return description;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+    public Patient getPatient() {
+        return patient;
+    }
+}
