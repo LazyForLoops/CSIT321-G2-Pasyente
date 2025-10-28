054f2a1845c117cac713570be61e324d933ff296
LAB24_USER ON WS41 <L24Y10W41@LabHCI.net>
2025-10-28 11:12:01 +0800

    setup(pasyente): add all required files

---
+++ src/main/java/com/csit321/appdev/pasyente/PasyenteApplication.java
@@ -0,0 +1,13 @@
+package com.csit321.appdev.pasyente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasyenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasyenteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/csit321/appdev/pasyente/PasyenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csit321.appdev.pasyente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasyenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
